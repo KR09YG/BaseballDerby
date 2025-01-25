a30a572c0e0b330a55c2fe7bcfbc78ca7e52c495
KRYG <abcdef0509a@gmail.com>
2025-01-25 21:42:41 +0900

    [update]弾道の処理に不具合があるため別の方法を使うことで修正

---
+++ Assets/Script/Batting.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Batting : MonoBehaviour
+{
+    [SerializeField] Image _cursor = default;
+    [SerializeField] Vector3 _size = Vector3.one;
+    [SerializeField] LayerMask _meetareaLayer = default;
+    [SerializeField] LayerMask _baseballLayer = default;
+    Rigidbody _rbBall;
+    Vector3 _center = default;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 10, _meetareaLayer))
+        {
+            _cursor.rectTransform.position = Input.mousePosition;
+            _center = hitInfo.point;
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var cols = Physics.OverlapBox(_center, _size, Quaternion.identity, _baseballLayer);
+
+            foreach (var c in cols)
+            {
+                _rbBall = c.GetComponent<Rigidbody>();
+
+                Vector3 reflection = Vector3.zero;
+
+                reflection.z = 5f;
+
+                if (c.transform.position.z > _center.z + 0.1f)
+                {
+                    reflection.x = Random.Range(-3,-1);
+                }
+                else if (c.transform.position.z < _center.z - 0.1f)
+                {
+                    reflection.x = Random.Range(0.1f,1f );
+                    reflection.z *= 1.5f;
+                }
+                else
+                {
+                    reflection.z *= 1.5f;
+                }
+
+                reflection.y = c.transform.position.y > _center.y ? 3f : -1f;
+
+
+                _rbBall.velocity = Vector3.zero;
+
+                Debug.Log(reflection);
+
+                _rbBall.AddForce(reflection * 8, ForceMode.Impulse);
+            }
+        }
+        if (_rbBall != null)
+        {
+            _rbBall.AddForce(new Vector3(0, -1f, 0));
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(_center, _size);
+    }
+}
